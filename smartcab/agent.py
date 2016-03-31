fb8495fa947a2ec2d61f05007c68cecc377f2b34
Jose <engtoolsdev@gmail.com>
2016-03-30 19:52:58 -0500

    State space tweaks

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -7,7 +7,7 @@ from collections import namedtuple
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
-    LearningAgentState = namedtuple('LearningAgentState', ['next_waypoint', 'light', 'oncoming', 'right', 'left', 'deadline'])
+    AState = namedtuple('AState', ['next_waypoint', 'light', 'deadline'])
 
     def __init__(self, env):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
@@ -28,29 +28,24 @@ class LearningAgent(Agent):
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
 
-        present_state = self.LearningAgentState(next_waypoint=self.next_waypoint, light=inputs['light'], oncoming=inputs['oncoming'], right=inputs['right'], left=inputs['left'], deadline=deadline)
-
+        present_state = self.AState(next_waypoint=self.next_waypoint, light=inputs['light'],  deadline=deadline)
         self.state = present_state
-
         old_q, action = self.choose_action(present_state)
-
         self.last_action = action
+
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # Sense new state
         new_inputs = self.env.sense(self)
-
-        new_state = self.LearningAgentState(next_waypoint=self.next_waypoint, light=new_inputs['light'], oncoming=new_inputs['oncoming'], right=new_inputs['right'], left=new_inputs['left'], deadline=deadline)
-
+        new_state = self.AState(next_waypoint=self.next_waypoint, light=new_inputs['light'],  deadline=deadline)
         new_Q = self.learned_val(reward, new_state, old_q)
-
         self.Q_values[(present_state, action)] = new_Q
 
         # print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}, oldQ = {}, newQ = {},".format(deadline, inputs, action, reward, old_q, new_Q)  # [debug]
 
     def learned_val(self, reward, new_state, old_q):
-        learning_rate = 0.4
+        learning_rate = 0.6
         discount_factor = 0.25
 
         new_q = old_q + (learning_rate * (reward + (discount_factor * self.maxQ(new_state) - old_q)))
@@ -63,12 +58,12 @@ class LearningAgent(Agent):
 
         return maxQ
 
-    def choose_action(self, state):
-        q = [self.getQ(state, a) for a in Environment.valid_actions]
+    def choose_action(self, state, valid_actions=Environment.valid_actions):
+        q = [self.getQ(state, a) for a in valid_actions]
         maxQ = max(q)
 
         if random.random() < 1:
-            best = [i for i in range(len(Environment.valid_actions)) if q[i] == maxQ]
+            best = [i for i in range(len(valid_actions)) if q[i] == maxQ]
             i = random.choice(best)
         else:
             i = q.index(maxQ)
@@ -83,7 +78,7 @@ class LearningAgent(Agent):
 class TrainedAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
-    LearningAgentState = namedtuple('LearningAgentState', ['next_waypoint', 'light', 'oncoming', 'right', 'left', 'deadline'])
+    LearningAgentState = namedtuple('LearningAgentState', ['next_waypoint', 'light',  'deadline'])
 
     def __init__(self, env, policy):
         super(TrainedAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
@@ -113,7 +108,7 @@ class TrainedAgent(Agent):
         deadline = self.env.get_deadline(self)
 
 
-        S = self.LearningAgentState(next_waypoint=self.next_waypoint, light=inputs['light'], oncoming=inputs['oncoming'], right=inputs['right'], left=inputs['left'], deadline=deadline)
+        S = self.LearningAgentState(next_waypoint=self.next_waypoint, light=inputs['light'], deadline=deadline)
 
         self.state = S
 
@@ -166,9 +161,11 @@ def run():
     a = e.create_agent(TrainedAgent, policy)  # create agent
     e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 
+    print(chr(27) + "[2J")
+
     # Now simulate it
     sim = Simulator(e, update_delay=0)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=25)  # press Esc or close pygame window to quit
+    sim.run(n_trials=100)  # press Esc or close pygame window to quit
 
 
 if __name__ == '__main__':
